2b74b507a170ec4cd625963b7065b79367f90d0e
Antoine Rey <antoine.rey@backmarket.com>
2021-04-11 23:28:17 +0200

    Feat: Create basic .cbz file

---
+++ README.md
@@ -0,0 +1,3 @@
+1. Avoid saving files to disk. Do it all in memory?
+2. Set correct informations (cover image, pages count…). Through headers?
+3. Automatically publish a new release.
--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"archive/zip"
 	"flag"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -13,11 +15,19 @@ import (
 )
 
 func main() {
+	var files []string
+
+	// ---
+	// Define the CLI flags.
+
 	flagBase := flag.String("base", "dist", "the base directory")
 	flagSerie := flag.String("serie", "-", "the serie")
 	flagChapter := flag.String("chapter", "1", "the chapter")
 
 	flag.Parse()
+
+	// ---
+	// Create the directory structure.
 
 	serie := strings.ReplaceAll(*flagSerie, " ", "-")
 	serie = strings.ReplaceAll(serie, "(", "")
@@ -25,11 +35,15 @@ func main() {
 	serie = strings.ToLower(serie)
 
 	directory := *flagBase + "/" + *flagSerie + "/" + *flagChapter
+	directoryTemp := directory + "/temp"
 
-	err := os.MkdirAll(directory, 0755)
+	err := os.MkdirAll(directoryTemp, 0755)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	// ---
+	// Download each images.
 
 	collector := colly.NewCollector(
 		// Only scrap the current page. Do not dive any deeper.
@@ -52,13 +66,45 @@ func main() {
 			log.Fatal(err)
 		}
 
-		file, err := os.Create(directory + "/" + filename)
+		file, err := os.Create(directoryTemp + "/" + filename)
 		if err != nil {
 			log.Fatal(err)
 		}
 
 		io.Copy(file, response.Body)
+		files = append(files, filename)
 	})
 
 	collector.Visit("https://readcomicsonline.ru/comic/" + serie + "/" + *flagChapter)
+
+	// ---
+	// Create the .cbz file.
+
+	filename := serie + "-" + *flagChapter + ".cbz"
+
+	zipFile, err := os.Create(directory + "/" + filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	zipWriter := zip.NewWriter(zipFile)
+	defer zipWriter.Close()
+
+	for _, file := range files {
+		content, err := ioutil.ReadFile(directoryTemp + "/" + file)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fileWriter, err := zipWriter.Create(file)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fileWriter.Write([]byte(string(content)))
+	}
+
+	// ---
+
+	fmt.Println("Done")
 }
